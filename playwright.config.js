5e63a11dc3d26416a073e824e08b6898d3e44c97
LesiaPavlenko <opavlenko@datagrok.ai>
2026-02-10 13:49:48 +0200

    Updating to chrome only

--- playwright.config.js
+++ playwright.config.js
@@ -29,10 +29,6 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    }
   ],
 });
